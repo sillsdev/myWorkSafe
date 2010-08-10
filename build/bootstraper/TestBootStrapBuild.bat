910f486e669ac1296fc5411d2911eadcb3b85374
John Hatton <hattonjohn@gmail.com>
2010-08-10 15:23:03 +1000

    Started bootstrapper builder (needed because of the two Microsoft MSI dependencies)

---
+++ build/bootstraper/TestBootStrapBuild.bat
@@ -0,0 +1,4 @@
+
+"dotNetInstaller 1.10\bin\InstallerLinker.exe" /c:"config.xml" /t:"dotNetInstaller 1.10\bin\dotNetInstaller.exe" /a:..\..\ /v /o:"..\..\output\installer\myWorkSafeInstaller.exe"
+
+PAUSE
