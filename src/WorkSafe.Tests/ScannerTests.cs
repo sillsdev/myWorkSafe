4c34b83e3a02020ce4c83548dca838d5396f7c74
John Hatton <hattonjohn@gmail.com>
2010-08-12 17:24:27 +1000

    Start of work to remove dependence on the Microsoft Sync Provider

---
+++ src/WorkSafe.Tests/ScannerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using myWorkSafe;
+using myWorkSafe.Groups;
+using NUnit.Framework;
+using Palaso.IO;
+using Palaso.TestUtilities;
+using System.IO;
+
+namespace WorkSafe.Tests
+{
+	[TestFixture]
+	public class ScannerTests
+	{
+		private DirectoryScanner _scanner;
+		[SetUp]
+		public void Setup()
+		{
+			_scanner = new DirectoryScanner();
+		}
+
+		[Test]
+		public void Test()
+		{
+			var files = _scanner.Scan(FileLocator.DirectoryOfApplicationOrSolution);
+			Assert.That(files.Count(), Is.GreaterThan(10));
+		}
+	}
+}
