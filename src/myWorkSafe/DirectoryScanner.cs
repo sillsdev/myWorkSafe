9deb0256235880b153db2519cece40ecfd3f42c4
hatton <hatton@localhost>
2010-08-12 17:24:27 +1000

    Start of work to remove dependence on the Microsoft Sync Provider

---
+++ src/myWorkSafe/DirectoryScanner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace myWorkSafe
+{
+	public class DirectoryScanner
+	{
+		public IEnumerable<FileOrDirectory> Scan(string rootPath)
+		{
+			yield return new FileOrDirectory() {Type = FileOrDirectory.FileOrDirectoryType.Directory, Path = rootPath};
+
+			foreach (var directory in Directory.GetDirectories(rootPath))
+			{
+				foreach(var item in Scan(directory))
+				{
+					yield return item; 
+				}
+			}
+			foreach (var file in Directory.GetFiles(rootPath))
+			{
+				yield return new FileOrDirectory() {Type = FileOrDirectory.FileOrDirectoryType.File, Path = file};
+			}
+		}
+	}
+
+	public struct FileOrDirectory
+	{
+		public string Path;
+		public enum FileOrDirectoryType {File, Directory}
+
+		public FileOrDirectoryType Type;
+
+	}
+}
