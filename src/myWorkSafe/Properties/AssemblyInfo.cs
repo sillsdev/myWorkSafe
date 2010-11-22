d96a45842da7d332ab934d208422d6c9ef209595
hatton <hatton@localhost>
2010-11-22 16:39:08 +1000

    Fix bug in msbuild script that caused improper assembly stamping on the build machine.

--- src/myWorkSafe/Properties/AssemblyInfo.cs
+++ src/myWorkSafe/Properties/AssemblyInfo.cs
@@ -22,15 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4d11d314-0a43-453a-a958-173bffacd0b2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+//these are set dynamically by the build process; the numbers here are ignored.
+[assembly: AssemblyVersion("1.1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1.1")]
